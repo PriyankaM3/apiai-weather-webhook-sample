71d586b3fe7a51c5b3efcba0be18ed50665cab48
PriyankaM3 <priya.mgdm@gmail.com>
2017-06-01 17:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,22 +36,63 @@ def webhook():
 
 
 def processRequest(req):
-    action = "open Life"
-if action == "open Life" :
- res = makeWebhookResult("Opening Life and annuities")
-   return res
-   
-else:
- action = "action is different"
-   return action
-   
-   
+    if req.get("result").get("action") != "yahooWeatherForecast":
+    res = "Action call successful"
+        return res
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-   
- return {
-        "speech": str(data),
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
